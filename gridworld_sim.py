a4528ed4579d7047e9cf3949f08e96f681db1650
Peter Mitrano <mitranopeter@gmail.com>
2017-08-20 01:41:26 -0400

    random agent in new gym env for grid world

--- gridworld_sim.py
+++ gridworld_sim.py
@@ -1,22 +1,25 @@
-import numpy as np
-import gym
-from gym.envs.classic_control import GridWorld
 from time import sleep
+
+import gym
+import numpy as np
+from gym.envs.classic_control import GridWorld
+from gym.envs.classic_control.gridworld import XY
 
 if __name__ == "__main__":
     env = gym.make("GridWorld-v0")
 
-    for i in range(1):
-        obs = env.reset()
-        j = 0
-        while True:
-            move = np.random.randint(0, 4)
-            env.render()
-            s0 = obs
-            obs, reward, done, info = env.step(move)
-            print(s0, GridWorld.actionToString(move), obs, reward)
-            sleep(10)
+    obs = env.reset()
+    print(obs)
+    env.render()
+    j = 0
+    sleep(1)
 
-            j += 1
-            if j > 10:
-                break
+    while True:
+        move = np.random.randint(0, 4)
+        s0 = XY(obs.x, obs.y)
+        obs, reward, done, info = env.step(move)
+        env.render()
+        print(j, s0, GridWorld.actionToString(move), obs, reward)
+        sleep(1)
+
+        j += 1
