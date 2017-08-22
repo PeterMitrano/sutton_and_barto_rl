7d55b5cac2fff13998b8fa13fc91e0ccfc670af4
Peter Mitrano <mitranopeter@gmail.com>
2017-08-22 01:35:13 -0400

    really not sure if there's a bug
    It may just be numerical instability, and nothing I can do about it

--- gamblers_problem.py
+++ gamblers_problem.py
@@ -9,7 +9,7 @@
 
 # This environment is episodic, and stochastic. The state is the amount of money the gambler has, and the available actions are to bet a certain amount of money. With some fixed percentage, the gambler will win and receive the amount of money bet, and otherwise loses the amount of money bet. The gambler's capital is between 1 and 99 dollars. The episode wins when the gambler has 0 or 100 dollars. Reward is zero on all transitions except those in which the gambler reaches \$100. Since this is episodic, we use no discount $\gamma$, so you won't see it in the code.
 
-# In[299]:
+# In[368]:
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,7 +18,7 @@ np.set_printoptions(precision=17, suppress=True)
 plt.style.use('bmh')
 
 
-# In[300]:
+# In[372]:
 
 converged = False
 p = 0.4
@@ -45,7 +45,7 @@ while not converged:
         v = V[s]
         V[s] = np.max(Q)
         delta = max(delta, abs(V[s] - v))
-        
+    
     Vs.append(V.copy())
     iters += 1
     
@@ -62,7 +62,7 @@ axes.set_ylim([0, 1])
 plt.plot(Vs[0], label="sweep 1")
 plt.plot(Vs[1], label="sweep 2")
 plt.plot(Vs[2], label="sweep 3")
-plt.plot(Vs[iters - 1], label="sweep {}".format(iters))
+plt.plot(Vs[-1], label="sweep {}".format(iters))
 plt.legend()
     
 plt.show()
@@ -70,11 +70,10 @@ plt.show()
 
 # ### Now the Final Policy
 
-# In[303]:
+# In[373]:
 
 pi = np.ndarray(100)
 Qs = [0]
-V = Vs[32]
 
 for s in range(1, 100):
     max_bet = min(s, 100 - s)
@@ -105,73 +104,19 @@ plt.show()
 
 # ### That graph looks funny
 # 
-# Notice how the graph doesn't exactly match the graph in the book? Well if you look carefully, it seems to be a numerical stability issue. Specifically, there are often two actions that miximize expected reward for each state. One is the maximum possible bet. I don't know why, so lets try to find out... recall $\gamma$ here is 1.0 so we ignore it.
+# First, notice how the graph doesn't exactly match the graph in the book. That's a numerical instability issue that I haven't fixed yet. But first let's explain why the way it should look is correct.
 # 
-# \begin{equation}
-# \begin{split}
-# V(s) &\leftarrow \max_a \sum_{s'} P^a_{ss'} * [R^a_{ss'} + V(s)] \\
-# V(s) &\leftarrow \max_a (p * [R^a_{s,s+a} + V(s + a)] + (1 - p) * [0 + V(s - a)]) \\
-# \end{split}
-# \end{equation}
+# I think the key is that you're usually going to lose. Whatever you bet, you're more likely to lose that amount. So essentially you should do the opposite of what you want to do.
 # 
-# consider when episode ends versus all other wins
-# $$ V(s) \leftarrow
-# \begin{cases}
-#    \max_a (p * [1 + V(s + a)] + (1 - p) * [0 + V(s - a)])& a + s = 100 \\
-#    \max_a (p * [0 + V(s + a)] + (1 - p) * [0 + V(s - a)])& a + s < 100 \\
-# \end{cases}
-# $$
-# drop some zeros
-# $$ V(s) \leftarrow
-# \begin{cases}
-#    \max_a (p * [1 + V(s + a)] + (1 - p) * V(s - a))& a + s = 100 \\
-#    \max_a (p * V(s + a) + (1 - p) * V(s - a))& a + s < 100 \\
-# \end{cases}
-# $$
-# 
-# If we have capital of < 50, then we cannot win, and we can ignore one of the conditions. In this event:
-# 
-# $$V(s) \leftarrow \max_a (p * V(s + a) + (1 - p) * V(s - a))$$
-# 
-# We can check our code and see if this is true:
+# For states below 50, you can never win, so you first need to get to 50 or higher. So when you're at $1, 2, ..., 12$, it makes sense to bet all you have. At 13 we bet 12. So why do we bet less after 13? Because you're trying to get back down to 13. So why is 13 is nice? Well if you bet 12 at 13, you get 25. Bet 25 at 25 and you get 50, then 100. Of course being at 80 is better than being at 50, at least according to our monotonically increasing state value function.
 
-# In[291]:
+# In[375]:
 
-s = 16
-Q16 = Qs[s]
-best_a = np.argmax(Q16)
-v1 = V[s]
-v2 = p * V[s + best_a] + (1 - p)*V[s - best_a]
-print(v1, v2, best_a)
-
-
-# Notice that there are two values with maximal expected value. for $s = 16$, they are 9 and 16. 16 of course corresponds to betting all your money. Let's look at that in the general case of $s < 50$ and $a == s$
-# 
-# \begin{equation}
-# \begin{split}
-# Q(s, a) &= p * V(s + a) + (1 - p) * V(s - a) \\
-# &= p * V(s + s) + (1 - p) * V(s - s) \\
-# &= p * V(s + s) + (1 - p) * V(0) \\
-# &= p * V(s + s) + (1 - p) * 0) \\
-# &= p * V(s + s)
-# \end{split}
-# \end{equation}
-# 
-# Why do 9 and 16 should have the same V? What about 8 and 17?
-
-# In[292]:
-
-# first element of each Q is just padding. You can't bet 0.
-Qs[16][1:]
-
-
-# In[304]:
-
-for s, Q in enumerate(Qs[1:]):
+for Q in Qs[1:]:
     Q = Q[1:]
-    plt.plot(Q, label="capital {}".format(s + 1))
+    plt.plot(Q)
 
-plt.xlabel("Capital")
+plt.xlabel("Capital (how much to bet)")
 plt.ylabel("Q Value")
 plt.title("\"Q Tree\" for Gambler's Problem p=0.4")
 plt.show()
@@ -191,26 +136,6 @@ plt.show()
 # \end{equation}
 # 
 # So as you get closer to 100, you get closer to that _sweet sweet_ reward of 1, so the action values get higher. The swoopyness still kind of confuses me. The fact that it is triangular, with the middle being the longest, is because at 50 you have the most possible actions/capital to bet.
-# 
-# ** What is that swoopy behavior about? **
-# 
-# At 50 capital, we bet 50, but as 51 we only be 1. Why?
-# 
-# \begin{equation}
-# \begin{split}
-# Q(50, 50) &= P^{50}_{50, 100} [ R^{50}_{50, 100} + V(100)] + P^{50}_{50, 0} [ R^{50}_{50, 0} + V(0)] \\ 
-# &=  0.4 * [1 + 0] + 0.6 * [0 + 0] \\
-# &=  0.4 \\
-# Q(51, 49) &= P^{49}_{51, 100} [ R^{49}_{51, 100} + V(100)] + P^{49}_{51, 1} [ R^{49}_{51, 1} + V(1)] \\ 
-# &=  0.4 * [1 + 0] + 0.6 * [0 + 0.002] \\
-# &=  0.4002 \\
-# \end{split}
-# \end{equation}
-
-# In[287]:
-
-V[1]
-
 
 # In[ ]:
 
